67fbaca77253e294b1714396ed28016d3467646f
aungkomyint222 <118752609+aungkomyint222@users.noreply.github.com>
2025-02-12 00:42:37 +0700

    f

--- app/taskest/page.js
+++ app/taskest/page.js
@@ -4,11 +4,11 @@ import TaskManager from './components/Tasklist'; // adjust the import path as ne
 
 export async function generateMetadata() {
     return {
-      title: "Tasket - Task Manager & Pomodoro Timer for Maximum Productivity",
+      title: "Taskest - Task Manager & Pomodoro Timer for Maximum Productivity",
       description:
-        "Boost your focus and efficiency with Tasket, the all-in-one task manager and Pomodoro timer by Aung Ko Myint. Stay organized, track tasks, and manage time effectively to get more done.",
+        "Boost your focus and efficiency with Taskest, the all-in-one task manager and Pomodoro timer by Aung Ko Myint. Stay organized, track tasks, and manage time effectively to get more done.",
       keywords:
-        "Tasket, task manager, Pomodoro timer, productivity app, time management, to-do list, work planner, focus timer, habit tracker, project management, best task manager, Aung Ko Myint, efficient work planning",
+        "Taskest, task manager, Pomodoro timer, productivity app, time management, to-do list, work planner, focus timer, habit tracker, project management, best task manager, Aung Ko Myint, efficient work planning",
     };
   }
 
